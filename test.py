4fb235006ff96164c62f90afdff0cdf3a9703139
Yao Ma <mayao@flip1.engr.oregonstate.edu>
2021-06-06 21:56:49 -0700

    added requirement txt, example, and test

---
+++ test.py
@@ -0,0 +1,10 @@
+import unittest
+import example
+
+class TestCase(unittest.Testcase):
+	
+	def test_add_1(self):
+		self.assertEqual(example.add(1,2), 3)
+
+if __name__ == '__main__':
+	unittest.main()
